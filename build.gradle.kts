69883fbf200fa608f4a96b806b7336693697be7e
Geek68 <njatovohyacinthe@gmail.com>
2025-05-03 09:08:44 +0300

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// build.gradle.kts (niveau projet)
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false  // Mise à jour vers Kotlin 1.9.10
+    id("com.android.library") version "8.1.0" apply false  // Ajouté pour la cohérence
+}
